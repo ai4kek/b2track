cf1115dd64bebe9705fbcf1f6a080ade92e7e7f5
Adeel Akram <adeel.chep@gmail.com>
2025-03-05 14:06:04 +0100

    Removing PerfFoM Moduel (use analysis-level track validation)

--- pg_track.py
+++ pg_track.py
@@ -21,60 +21,6 @@ import svd
 import tracking as trkx
 from ROOT import Belle2
 
-
-# Maybe subclassing TrackingPerformanceEvalutionModule works?
-class PerfFoM(b2.Module):
-    """The basf2 module to extract performance FoM."""
-
-    def __init__(self):
-        super().__init__()
-
-        # add variables if needed here:
-
-    def initialize(self):
-        """Create a member to access event info and the MCParticles
-        StoreArray
-        """
-        #: an example object from the datastore (the metadata collection for the event)
-        self.eventinfo = Belle2.PyStoreObj("EventMetaData")
-
-        #: an example array from the datastore (the list of MC particles)
-        self.particles = Belle2.PyStoreArray("MCParticles")
-
-    def beginRun(self):
-        """Called every time a run changes before the actual events in that run
-        are processed
-        """
-        b2.B2INFO("beginRun()")
-
-    def event(self):
-        """Print the number of charged particles and the total charge"""
-        n_charged = 0
-        total_charge = 0
-        for particle in self.particles:
-            charge = particle.getCharge()
-            if charge:
-                n_charged += 1
-            total_charge += charge
-
-        b2.B2INFO(
-            f"Number of charged particles = {n_charged}, "
-            f"total charge of event = {total_charge}"
-        )
-
-    def endRun(self):
-        """Called every time a run changes after the actual events in that run
-        were processed
-        """
-        b2.B2INFO("endRun()")
-
-    def terminate(self):
-        """Called once after all the processing is complete"""
-        b2.B2INFO("terminate()")
-
-
-##########################################################################
-
 # Create the steering path
 main = b2.Path()
 
@@ -117,9 +63,6 @@ for module in main.modules():
     if module.name() == "ToCDCCKF":
         # module.param("maximalLayerJump", 6)  # change a parameter
         b2.print_params(module, print_values=True, shared_lib_path=None)
-
-# TODO: Add FoM module to get plots
-main.add_module(PerfFoM())
 
 # Create the mDST output file
 additional_br = []
