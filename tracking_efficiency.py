bc1d9f45b5dab11c6230a44378b86b2997c527cf
Adeel Akram <adeel.chep@gmail.com>
2025-04-09 13:10:08 +0200

    Better Tracking Efficiency

--- tracking_efficiency.py
+++ tracking_efficiency.py
@@ -7,152 +7,98 @@ from ROOT import Belle2
 
 
 class TrackingMetrics(basf2.Module):
-    """Calculate Tracking Efficiency and Purity from the mDST."""
-
     def initialize(self):
-        self.matched_mc_particles = 0
-        self.total_mc_particles = 0
-        self.matched_reco_tracks = 0
-        self.total_reco_tracks = 0
-        return 0
+        """initialize"""
 
-    def event(self):
-
-        # Get StoreArray of MCParticles
-        mc_particles = Belle2.PyStoreArray("MCParticles")
-        self.total_mc_particles += mc_particles.getEntries()
-
-        # Get StoreArray of Tracks or TrackFitResults
-        reco_tracks = Belle2.PyStoreArray("Tracks")
-        self.total_reco_tracks += reco_tracks.getEntries()
-
-        # Count matched Tracks
-        for track in reco_tracks:
-            # track_to_particle = track.getRelatedTo("MCParticles")
-            track_to_particle = track.getRelated("MCParticles")
-
-            # it relation exists (not nullptr), there might a better way to check
-            if track_to_particle:
-                self.matched_reco_tracks += 1
-
-        # Count matched MC Particles
-        for particle in mc_particles:
-            # particle_to_track = particle.getRelatedFrom("Tracks")
-            particle_to_track = particle.getRelated("Tracks")
-
-            # it relation exists (not nullptr), there might a better way to check
-            if particle_to_track:
-                # print(f"Relation name: {particle_to_track.GetName()}")
-                self.matched_mc_particles += 1
-
-        return 0
-
-    def terminate(self):
-
-        # overall purity
-        purity = 0
-        if self.total_reco_tracks > 0:
-            purity = self.matched_reco_tracks / self.total_reco_tracks
-
-        print(f"\nTracking Purity: {purity:.4f}")
-        print(f"Matched reconstructed tracks: {self.matched_reco_tracks}")
-        print(f"Total reconstructed tracks: {self.total_reco_tracks}")
-
-        # overall efficiency
-        efficiency = 0
-        if self.total_mc_particles > 0:
-            efficiency = self.matched_mc_particles / self.total_mc_particles
-
-        print(f"\nTracking Efficiency: {efficiency:.4f}")
-        print(f"Matched MC particles: {self.matched_mc_particles}")
-        print(f"Total MC particles: {self.total_mc_particles}")
-
-        return 0
-
-
-class TrackingMetrics_old(basf2.Module):
-    def initialize(self):
+        # Particles StoreArray
+        self.MCParticles = Belle2.PyStoreArray("MCParticles")
+        self.MCParticles.isRequired()
 
         # Tracks StoreArray
-        # self.reco_tracks = Belle2.PyStoreArray('Tracks')
-        # self.reco_tracks.isRequired()
-
-        # MCParticles StoreArray
-        # self.mc_particles = Belle2.PyStoreArray('MCParticles')
-        # self.mc_particles.isRequired()
-
-        self.val_counter1 = 0
-        self.val_counter2 = 0
-
-        # counters
-        self.matched_mc_particles = 0
-        self.total_mc_particles = 0
-
-        self.total_reconstructable_mc = 0
-
-        self.matched_reco_tracks = 0
-        self.total_reco_tracks = 0
-        return 0
-
-    def event(self):
-
-        # Get StoreArray of MCParticles
-        mc_particles = Belle2.PyStoreArray("MCParticles")
-        self.total_mc_particles += mc_particles.getEntries()
-        # self.total_mc_particles += len(mc_particles)
-
-        # Get StoreArray of Tracks or TrackFitResults
-        reco_tracks = Belle2.PyStoreArray("Tracks")
-        self.total_reco_tracks += reco_tracks.getEntries()
-        # self.total_reco_tracks += len(reco_tracks)
+        self.Tracks = Belle2.PyStoreArray("Tracks")
+        self.Tracks.isRequired()
 
         # Get relations (How to access and use?)
         # relations = Belle2.PyStoreArray('TracksToMCParticles')
 
-        # tracking/tests/track_to_mcparticle_relation_test.py
-        for track in reco_tracks:
-            track_to_particle_relations = track.getRelationsTo("MCParticles")
-            n_relations = track_to_particle_relations.size()
-            if n_relations == 0:
-                pass  # print(f"No. of relations from Tracks to MCParticles: {n_relations}")
+        # counters
+        self.matched_mc_particles = 0
+        self.selected_mc_particles = 0
+        self.total_mc_particles = 0
 
-        for particle in mc_particles:
-            particle_to_track_relation = particle.getRelationsTo("Tracks")
-            n_relations = particle_to_track_relation.size()
-            if n_relations == 0:
-                pass  # print(f"No. of relations from MCParticles to Tracks: {n_relations}")
+        self.matched_reco_tracks = 0
+        self.total_reco_tracks = 0
 
-        # TODO: Purity: Number of matched reconstructed tracks divided by total
+        return 0
+
+    def event(self):
+        """event loop"""
+
+        # count total
+        self.total_mc_particles += self.MCParticles.getEntries()
+        self.total_reco_tracks += self.Tracks.getEntries()
+
+        # Tracking Purity: Number of matched reconstructed tracks divided by total
         # reconstructed tracks. We already have 'total_reco_tracks', but need to
         # find 'matched_reco_tracks' by counting reconstructed tracks that are
         # matched to an mc particle per event, then aggregate for all events.
 
         # Count matched Tracks
-        for track in reco_tracks:
-            track_to_particle = track.getRelated("MCParticles")  # getRelatedTo()
+        for track in self.Tracks:
+            # track_to_particle_relations = track.getRelationsTo("MCParticles")
+            # track_to_particle_relation = track.getRelatedTo("MCParticles")
+            track_to_particle = track.getRelated("MCParticles")
 
             # it relation exists (not nullptr), there might a better way to check
             if track_to_particle:
                 # print(f"Relation name: {track_to_particle.GetName()}")
                 self.matched_reco_tracks += 1
 
-        # TODO: Efficiency: Number of matched mc particles divided by total
+        # Tracking Efficiency: Number of matched mc particles divided by total
         # generated particles. We already have 'total_mc_particles', but need to
         # find 'matched_mc_particles' by counting mc particles that are matched
         # to a reconstructed track per event, then aggregate for all events.
 
         # Count matched MC Particles
-        for particle in mc_particles:
-            particle_to_track = particle.getRelated("Tracks")  # getRelatedFrom()
+        for mc in self.MCParticles:
 
-            # it relation exists (not nullptr), there might a better way to check
-            if particle_to_track:
-                # print(f"Relation name: {particle_to_track.GetName()}")
-                self.matched_mc_particles += 1
+            # selected criteria
+            isChargedPion = abs(mc.getPDG()) == 211  # only charged pions
+            isCharged = mc.getCharge() != 0  # only charged particles
+            isPrimary = mc.hasStatus(1)  # only primary particles
+            isStable = mc.hasStatus(2)  # only stable particles
+            isSeen = mc.hasSeenInDetector(  # only seen in CDC
+                Belle2.Const.DetectorSet(Belle2.Const.CDC)
+            )
+
+            # Primary, charged pions seen in CDC
+            isGoodParticle = isPrimary and isChargedPion and isSeen
+
+            if isGoodParticle:
+
+                # count selected particles
+                self.selected_mc_particles += 1
+
+                # matched to a track with no clones (FIXME: No clones?)
+                # particle_to_track_relations = mc.getRelationsFrom("Tracks")
+                # particle_to_track_relation = mc.getRelatedFrom("Tracks")
+                particle_to_track_relation = mc.getRelated("Tracks")
+                if particle_to_track_relation:
+                    self.matched_mc_particles += 1
 
         return 0
 
     def terminate(self):
+        """terminate"""
+        # overall efficiency
+        efficiency = 0
+        if self.total_mc_particles > 0:
+            efficiency = self.matched_mc_particles / self.selected_mc_particles
+
+        print(f"\nTracking Efficiency: {efficiency:.4f}")
+        print(f"Matched MC particles: {self.matched_mc_particles}")
+        print(f"Selected MC particles: {self.selected_mc_particles}")
+        print(f"Total MC particles: {self.total_mc_particles}")
 
         # overall purity
         purity = 0
@@ -163,16 +109,7 @@ class TrackingMetrics_old(basf2.Module):
         print(f"Matched reconstructed tracks: {self.matched_reco_tracks}")
         print(f"Total reconstructed tracks: {self.total_reco_tracks}")
 
-        # overall efficiency
-        efficiency = 0
-        if self.total_mc_particles > 0:
-            efficiency = self.matched_mc_particles / self.total_mc_particles
-
-        print(f"\nTracking Efficiency: {efficiency:.4f}")
-        print(f"Matched MC particles: {self.matched_mc_particles}")
-        print(f"Total MC particles: {self.total_mc_particles}")
-
-        # Save metrics to file
+        # save metrics to file
         with open("track_metrics.csv", "w") as f:
             f.write("metric,value\n")
             f.write(f"efficiency,{efficiency}\n")
