1e39d0779c6a808afa092386be970868478aca80
Adeel Akram <adeel.chep@gmail.com>
2025-09-10 17:29:14 +0200

    Path fixing

--- tracking_validation.sh
+++ tracking_validation.sh
@@ -8,14 +8,15 @@ source /cvmfs/belle.cern.ch/tools/b2setup release-08-03-00
 
 
 # sample name
-output_prefix=mixed_hpo
+output_prefix=mixed
 
 # generate ntuples from mDST > ./validation/mixed_ntuple.root
 echo "generating ntuples from mDST..."
-basf2 tracking_performance.py -- -p "$output_prefix" -i "dataset/mixed_rec.root" 2>&1 | tee "validation/${output_prefix}.log"
+basf2 tracking_performance.py -- -p "$output_prefix" -i "dataset/mixed_rec.root"
 echo "tracking_performance.py script executed successfully..."
 
 # generate validation metrics > ./validation/mixed_hist.root
 echo "generating validation metrics..."
-basf2 tracking_validation.py -- -p1 "validation/$output_prefix" -f1 "validation/${output_prefix}_ntuple.root"
+basf2 tracking_validation.py -- -p1 "validation/$output_prefix" -f1 "validation/${output_prefix}_ntuple.root" 2>&1 | tee "validation/${output_prefix}.log"
+
 echo "tracking_validation.py script executed successfully..."
